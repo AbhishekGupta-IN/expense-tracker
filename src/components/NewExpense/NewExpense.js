bb03321f95b0756b9284481219d3b9f9d6c968cd
Abhishek Gupta <abhishekgupt.in@gmail.com>
2021-06-01 17:42:59 +0530

    Added conditionals and chart componenets

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isSelected, setIsSelected] = useState(false);
+
   const saveExpenseHandler = (inputExpenseDate) => {
     const expenseData = {
       ...inputExpenseDate,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsSelected(false);
   };
+
+  const showForm = (event) => {
+    setIsSelected(true);
+  };
+
+  const noForm = (event) => {
+    setIsSelected(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+      {!isSelected && <button onClick={showForm}>Add New Expense</button>}
+      {isSelected && (
+        <ExpenseForm onSaveExpenseData={saveExpenseHandler} onCancel={noForm} />
+      )}
     </div>
   );
 };
